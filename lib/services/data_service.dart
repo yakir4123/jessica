9dd3f9351212a0407a27cdd590589197ede50eb6
yinonh <yinon.h21@gmail.com>
2024-08-02 13:06:32 +0300

    bugfix: when the graph not contain data it show text instead and steam reconnect on resume

--- lib/services/data_service.dart
+++ lib/services/data_service.dart
@@ -1,28 +1,44 @@
 import 'dart:convert';
+import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
-class DataService extends StateNotifier<Map<String, dynamic>> {
-  final WebSocketChannel _channel;
+class DataService extends StateNotifier<Map<String, dynamic>>
+    with WidgetsBindingObserver {
+  WebSocketChannel? _channel;
   Map<String, dynamic> decodedMessage = {};
   String selectedKey = "LiveStrategy:Binance Perpetual Futures:AVAX-USDT";
 
-  DataService()
-      : _channel = WebSocketChannel.connect(
-          Uri.parse(
-              "ws://${dotenv.env["JESSE_SERVER_IP"]}:${dotenv.env["JESSE_SERVER_PORT"]}/ws"),
-        ),
-        super({}) {
-    _channel.stream.listen((message) {
-      final decodedMessage = json.decode(message) as Map<String, dynamic>;
-      this.decodedMessage = decodedMessage;
-      if (decodedMessage.containsKey(selectedKey)) {
-        state = Map<String, dynamic>.from(decodedMessage[selectedKey]);
-      } else {
-        state = {};
-      }
-    });
+  DataService() : super({}) {
+    WidgetsBinding.instance.addObserver(this);
+    _connectWebSocket();
+  }
+
+  void _connectWebSocket() {
+    _channel = WebSocketChannel.connect(
+      Uri.parse(
+          "ws://${dotenv.env["JESSE_SERVER_IP"]}:${dotenv.env["JESSE_SERVER_PORT"]}/ws"),
+    );
+
+    print('WebSocket stream started');
+    _channel!.stream.listen(
+      (message) {
+        final decodedMessage = json.decode(message) as Map<String, dynamic>;
+        this.decodedMessage = decodedMessage;
+        if (decodedMessage.containsKey(selectedKey)) {
+          state = Map<String, dynamic>.from(decodedMessage[selectedKey]);
+        } else {
+          state = {};
+        }
+      },
+      onDone: () {
+        print('WebSocket stream ended');
+      },
+      onError: (error) {
+        print('WebSocket stream error: $error');
+      },
+    );
   }
 
   void selectKey(String key) {
@@ -36,7 +52,21 @@ class DataService extends StateNotifier<Map<String, dynamic>> {
 
   @override
   void dispose() {
-    _channel.sink.close();
+    WidgetsBinding.instance.removeObserver(this);
+    print('Disposing DataService');
+    _channel?.sink.close();
     super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    super.didChangeAppLifecycleState(state);
+    if (state == AppLifecycleState.resumed) {
+      print('App resumed, reconnecting to WebSocket');
+      _connectWebSocket();
+    } else if (state == AppLifecycleState.paused) {
+      print('App paused, closing WebSocket connection');
+      _channel?.sink.close();
+    }
   }
 }
