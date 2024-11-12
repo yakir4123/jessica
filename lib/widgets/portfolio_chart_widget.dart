ba726576af9999f3af428a5c6e62b02fdb1e8382
Yakir <yakir4123@gmail.com>
2024-11-12 16:16:50 +0200

    1 legend for both portfolio charts

--- lib/widgets/portfolio_chart_widget.dart
+++ lib/widgets/portfolio_chart_widget.dart
@@ -3,19 +3,34 @@ import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 import 'package:jessica/services/portfolio_weights_provider.dart';
 
-class PortfolioChartWidget extends StatelessWidget {
+class PortfolioChartWidget extends StatefulWidget {
   final Map<num, Map<String, dynamic>> symbolizeDf;
 
   const PortfolioChartWidget({super.key, required this.symbolizeDf});
 
   @override
+  _PortfolioChartWidgetState createState() => _PortfolioChartWidgetState();
+}
+
+class _PortfolioChartWidgetState extends State<PortfolioChartWidget> {
+  late List<_ChartData> lineChartData;
+  late List<_PieData> pieChartData;
+  late List<String> symbols;
+  late Map<String, bool> selectedSymbols;
+
+  @override
+  void initState() {
+    super.initState();
+    lineChartData = _generateLineChartData(widget.symbolizeDf);
+    pieChartData = _generatePieChartData(widget.symbolizeDf);
+    symbols = {for (var item in lineChartData) item.symbol}.toList();
+
+    // Initialize all symbols as visible
+    selectedSymbols = {for (var symbol in symbols) symbol: true};
+  }
+
+  @override
   Widget build(BuildContext context) {
-    List<_ChartData> lineChartData = _generateLineChartData(symbolizeDf);
-    List<_PieData> pieChartData = _generatePieChartData(symbolizeDf);
-
-    List<String> strategyIds =
-        {for (var item in lineChartData) item.symbol}.toList();
-
     return SizedBox(
       height: 700,
       child: Column(
@@ -24,47 +39,71 @@ class PortfolioChartWidget extends StatelessWidget {
           // Pie Chart
           Expanded(
             child: SfCircularChart(
-              legend: Legend(
-                isVisible: true,
-                position: LegendPosition.bottom,
-                textStyle: const TextStyle(color: Colors.white),
-                overflowMode: LegendItemOverflowMode.wrap,
-              ),
               series: <CircularSeries>[
                 PieSeries<_PieData, String>(
-                    dataSource: pieChartData,
-                    xValueMapper: (_PieData data, _) => data.symbol,
-                    yValueMapper: (_PieData data, _) => data.value,
-                    pointColorMapper: (_PieData data, _) =>
-                        _getColor(data.symbol)),
+                  dataSource: pieChartData
+                      .where((data) => selectedSymbols[data.symbol]!)
+                      .toList(),
+                  xValueMapper: (_PieData data, _) => data.symbol,
+                  yValueMapper: (_PieData data, _) => data.value,
+                  pointColorMapper: (_PieData data, _) =>
+                      _getColor(data.symbol),
+                ),
               ],
-              tooltipBehavior:
-                  TooltipBehavior(enable: true), // Tooltip on hover
+              tooltipBehavior: TooltipBehavior(enable: true),
             ),
           ),
           const SizedBox(height: 16.0), // Spacing between charts
+          // Stacked Area Chart
           SfCartesianChart(
-            legend: Legend(isVisible: true, position: LegendPosition.bottom),
             primaryXAxis: DateTimeAxis(),
-            // Ensures DateTime values are expected on the X-axis
             primaryYAxis: NumericAxis(
               minimum: 0,
               maximum: 1,
             ),
             series: <ChartSeries>[
-              for (String strategyId in strategyIds)
-                StackedAreaSeries<_ChartData, DateTime>(
+              for (String symbol in symbols)
+                if (selectedSymbols[symbol]!)
+                  StackedAreaSeries<_ChartData, DateTime>(
                     dataSource: lineChartData
-                        .where((data) => data.symbol == strategyId)
+                        .where((data) => data.symbol == symbol)
                         .toList(),
                     xValueMapper: (_ChartData data, _) => data.timestamp,
                     yValueMapper: (_ChartData data, _) => data.weight,
                     pointColorMapper: (_ChartData data, _) =>
-                        _getColor(data.symbol)),
+                        _getColor(data.symbol),
+                    name: symbol,
+                  ),
             ],
             tooltipBehavior: TooltipBehavior(enable: true),
-          )
-          // Stacked Area Chart
+          ),
+          const SizedBox(height: 16.0), // Spacing
+          Wrap(
+            alignment: WrapAlignment.center,
+            children: symbols.map((symbol) {
+              return GestureDetector(
+                onTap: () {
+                  setState(() {
+                    selectedSymbols[symbol] = !selectedSymbols[symbol]!;
+                  });
+                },
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(
+                      selectedSymbols[symbol]!
+                          ? Icons.check_box
+                          : Icons.check_box_outline_blank,
+                      color: _getColor(symbol),
+                    ),
+                    const SizedBox(width: 4),
+                    Text(symbol, style: const TextStyle(color: Colors.white)),
+                    const SizedBox(width: 8),
+                  ],
+                ),
+              );
+            }).toList(),
+          ),
         ],
       ),
     );
@@ -101,24 +140,24 @@ class PortfolioChartWidget extends StatelessWidget {
   Color _getColor(String symbol) {
     final Random random = Random();
     return {
-      'TRX-USDT': Colors.red,
-      'UNI-USDT': Colors.pinkAccent,
-      'SAND-USDT': Colors.yellow,
-      'LINK-USDT': Colors.deepPurple,
-      'HBAR-USDT': Colors.black26,
-      'GRT-USDT': Colors.deepPurpleAccent,
-      'ETH-USDT': Colors.blueGrey,
-      'DOT-USDT': Colors.pink,
-      'DOGE-USDT': Colors.brown,
-      'CHZ-USDT': Colors.redAccent,
-      'DASH-USDT': Colors.lightBlueAccent,
-
-    }[symbol] ?? Color.fromARGB(
-      255,
-      random.nextInt(256), // Random red value
-      random.nextInt(256), // Random green value
-      random.nextInt(256), // Random blue value
-    );
+          'TRX-USDT': Colors.red,
+          'UNI-USDT': Colors.pinkAccent,
+          'SAND-USDT': Colors.yellow,
+          'LINK-USDT': Colors.deepPurple,
+          'HBAR-USDT': Colors.black26,
+          'GRT-USDT': Colors.deepPurpleAccent,
+          'ETH-USDT': Colors.blueGrey,
+          'DOT-USDT': Colors.pink,
+          'DOGE-USDT': Colors.brown,
+          'CHZ-USDT': Colors.redAccent,
+          'DASH-USDT': Colors.lightBlueAccent,
+        }[symbol] ??
+        Color.fromARGB(
+          255,
+          random.nextInt(256), // Random red value
+          random.nextInt(256), // Random green value
+          random.nextInt(256), // Random blue value
+        );
   }
 }
 
